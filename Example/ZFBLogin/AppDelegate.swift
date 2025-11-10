8811dc857c480f13f44df7bd619db49c04ec5364
cbzhang <wbzhangcb@centaline.com.cn>
2025-11-10 10:27:41 +0800

    test 2

--- Example/ZFBLogin/AppDelegate.swift
+++ Example/ZFBLogin/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("test 111")
+        print("test 111 222")
         return true
     }
 
